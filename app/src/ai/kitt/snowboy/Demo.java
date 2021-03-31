c48de9c125fd75d53aa3afb410812f71c5466de6
KIM <whdals9711@gmail.com>
2021-03-31 09:05:48 +0900

    SMS send and GPS

--- app/src/ai/kitt/snowboy/Demo.java
+++ app/src/ai/kitt/snowboy/Demo.java
@@ -2,15 +2,32 @@ package ai.kitt.snowboy;
 
 import ai.kitt.snowboy.audio.RecordingThread;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import ai.kitt.snowboy.audio.AudioDataSaver;
 import ai.kitt.snowboy.demo.R;
@@ -21,6 +38,9 @@ public class Demo extends Activity {
 
     private Button record_button;
     private Button play_button;
+
+    private SmsSend smsSend;
+    private GpsTracker gpsTracker;
 
     private static long activeTimes = 0;
 
@@ -92,6 +112,16 @@ public class Demo extends Activity {
     private OnClickListener selfsue_button_handle = new OnClickListener() {
         @Override
         public void onClick(View v) {
+            smsSend = new SmsSend(Demo.this);
+            String msg = "살려주세요! 제 위치는 ";
+            String phnum = "01026670860";
+            gpsTracker = new GpsTracker(Demo.this);
+            double latitude = gpsTracker.getLatitude();
+            double longitude =gpsTracker.getLongitude();
+            String loc = getCurrentAddress(latitude,longitude);
+            smsSend.sendMsg(msg+loc+" 입니다.",phnum);
+
+
 
         }
     };
@@ -134,6 +164,43 @@ public class Demo extends Activity {
              }
         }
     };
+
+    public String getCurrentAddress( double latitude, double longitude) {
+
+        //지오코더... GPS를 주소로 변환
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        List<Address> addresses;
+
+        try {
+
+            addresses = geocoder.getFromLocation(
+                    latitude,
+                    longitude,
+                    7);
+        } catch (IOException ioException) {
+            //네트워크 문제
+            Toast.makeText(this, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show();
+            return "지오코더 서비스 사용불가";
+        } catch (IllegalArgumentException illegalArgumentException) {
+            Toast.makeText(this, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show();
+            return "잘못된 GPS 좌표";
+
+        }
+
+
+
+        if (addresses == null || addresses.size() == 0) {
+            Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show();
+            return "주소 미발견";
+
+        }
+
+        Address address = addresses.get(0);
+        return address.getAddressLine(0).toString()+"\n";
+
+    }
+
 
     @Override
      public void onDestroy() {
---
+++ app/src/ai/kitt/snowboy/SmsSend.java
@@ -0,0 +1,25 @@
+package ai.kitt.snowboy;
+
+import android.content.Context;
+import android.telephony.SmsManager;
+import android.widget.Toast;
+
+public class SmsSend {
+    private final Context mContext;
+
+    public SmsSend(Context context){
+        this.mContext = context;
+    }
+
+    public void sendMsg(String msg, String phoneNo){
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo,null,msg,null,null);
+            Toast.makeText(mContext, "전송 완료!", Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+            Toast.makeText(mContext, e.toString(), Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+    }
+}
