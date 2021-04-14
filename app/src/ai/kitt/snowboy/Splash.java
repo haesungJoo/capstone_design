340d0078b1db4601cf0546b21632ec8219290564
LuLuLaLaLa <haesung0728@gmail.com>
2021-04-14 13:17:58 +0900

    - Splash Activity 추가

---
+++ app/src/ai/kitt/snowboy/Splash.java
@@ -0,0 +1,30 @@
+package ai.kitt.snowboy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+import ai.kitt.snowboy.demo.R;
+
+public class Splash extends Activity {
+
+    private final static int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent spIntent = new Intent(Splash.this, Demo.class);
+                startActivity(spIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
